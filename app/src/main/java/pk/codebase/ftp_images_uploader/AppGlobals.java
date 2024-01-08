45c13a0bf1aaa8cab9d96c59b1b52be408d00033
Shahid Ghafoor <shahid@thing.com>
2024-01-08 15:15:02 +0500

    cleanup

--- app/src/main/java/pk/codebase/ftp_images_uploader/AppGlobals.java
+++ app/src/main/java/pk/codebase/ftp_images_uploader/AppGlobals.java
@@ -1,17 +1,8 @@
 package pk.codebase.ftp_images_uploader;
 
-
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.net.ConnectivityManager;
-import android.net.Uri;
-import android.os.Environment;
-import android.preference.PreferenceManager;
-import android.provider.OpenableColumns;
 
-import java.io.File;
 
 public class AppGlobals extends Application {
     public static Context sContext;
